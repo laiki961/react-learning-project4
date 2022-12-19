ecf43aec7d077f7b63d4e4ab92dfe8b468457f0f
laiki961 <laiki961@gmail.com>
2022-12-18 20:13:50 -0500

    created a custom Context Provider Component (a separate Context Management component) called "AuthContextProvider"

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+//These are the default values, adding properties into this js would allow the VSCode auto-completion
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
