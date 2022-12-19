cd5344d7f2f1be95c9b806782a0dc108ab3468a8
laiki961 <laiki961@gmail.com>
2022-12-18 19:41:59 -0500

    Applied the same on Logout

--- src/App.js
+++ src/App.js
@@ -28,8 +28,10 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-      <MainHeader onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+    >
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import AuthContext from "../../store/auth-context";
 import classes from "./Navigation.module.css";
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
@@ -19,7 +19,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
